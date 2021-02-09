1ab996a558a593b8ecedd8c2d8cb828491e93e0c
jhokes <72086491+jhokes@users.noreply.github.com>
2021-02-08 23:27:31 -0500

    Add files via upload

---
+++ todo/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'WorkWork11',
+    database: 'todo'
+});
+
+try{
+    connection.connect();
+} catch (e) {
+console.log(oh no);
+console.log(e);
+}
+const api = express();
+api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json());
+
+api.listen(3000, () => {
+    console.log('API up and running!');
+});
+
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('It works!');
+});
